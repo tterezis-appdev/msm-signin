5db93f8c02db6a74c604769067735a724adeac82
Teddy Terezis <tterezis@chicagobooth.edu>
2023-05-16 02:39:03 +0000

    Sign up page working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_15_235816) do
+ActiveRecord::Schema.define(version: 2023_05_16_022221) do
 
   create_table "actors", force: :cascade do |t|
     t.string "name"
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 2023_05_15_235816) do
     t.integer "director_id"
     t.text "description"
     t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "password_digest"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
